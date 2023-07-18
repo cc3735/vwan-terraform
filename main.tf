705665c004e37bfe38f52673fb24cc5884c72afd
cc3735 <104941217+cc3735@users.noreply.github.com>
2023-07-18 14:15:04 -0400

    major update to code
    Stuff happened

--- main.tf
+++ main.tf
@@ -11,6 +11,19 @@ provider "azurerm" {
   features{}
 }
 
+locals {
+  vwan-rg = values({for k, v in var.resource_groups : k => v.name if k == "vwan-rg" })
+  vwanhub-east-rg = values({for k, v in var.resource_groups : k => v.name if k == "vwanhub-eastus2-rg" })
+  vwanhub-central-rg = values({for k, v in var.resource_groups : k => v.name if k == "vwanhub-central-rg" })
+  dmzspoke1-east-rg = values({for k, v in var.resource_groups : k => v.name if k == "dmzspoke1-eastus2-rg" })
+  dmzspoke1-central-rg = values({for k, v in var.resource_groups : k => v.name if k == "dmzspoke1-central-rg" })
+  subscriptions = {
+    "westus" = "my-westus-subscription-id"
+    "eastus" = "my-eastus-subscription-id"
+    "centralus" = "my-centralus-subscription-id"
+  }
+} 
+
 
 resource "azurerm_resource_group" "resource_groups" {
   for_each = var.resource_groups
@@ -28,18 +41,25 @@ resource "azurerm_virtual_wan" "vwan" {
 
 resource "azurerm_virtual_hub" "vhub" {
   for_each = var.vhub
-  name                = each.key
+  name                = each.value.name
   resource_group_name = each.value.rgname
   location            = each.value.location
   virtual_wan_id      = azurerm_virtual_wan.vwan.id
   address_prefix      = each.value.address_prefix
-  dynamic "route" {
-    for_each = var.route
-    content {
-      address_prefixes = route.value.address_prefixes
-      next_hop_ip_address = route.value.next_hop_ip_address
-    }
-  }
+#  dynamic "route" {
+#    for_each = var.route
+#    content {
+#      address_prefixes = route.value.address_prefixes
+#      next_hop_ip_address = route.value.next_hop_ip_address
+#    }
+#  }
+}
+
+
+data "azurerm_virtual_hub" "vhub" {
+  for_each = azurerm_virtual_hub.vhub
+  name = each.value.name
+  resource_group_name = each.value.rgname
 }
 
 
@@ -53,25 +73,22 @@ resource "azurerm_express_route_gateway" "ergateway" {
   scale_units =  5
   }
 
-
-# We need to import the existing express route port example 
-
+# We need to import the existing express route port 
 # terraform import azurerm_express_route_port.example /subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/group1/providers/Microsoft.Network/expressRoutePorts/port1
 
-#####
 
-data "azurerm_express_route_circuit" "example" {
+data "azurerm_express_route_circuit" "er_circuits" {
   for_each = azurerm_express_route_gateway.ergateway
   resource_group_name = each.value.resource_group_name
   name = each.key
 } 
 
 
-resource "azurerm_express_route_circuit_peering" "example" {
+resource "azurerm_express_route_circuit_peering" "er_peering" {
   for_each = azurerm_express_route_gateway.ergateway
   peering_type                  = "AzurePrivatePeering"
-  express_route_circuit_name    = "${data.azurerm_express_route_circuit.example[each.key].name}"
-  resource_group_name           = "${data.azurerm_express_route_circuit.example[each.key].resource_group_name}"
+  express_route_circuit_name    = "${data.azurerm_express_route_circuit.er_circuits[each.key].name}"
+  resource_group_name           = "${data.azurerm_express_route_circuit.er_circuits[each.key].resource_group_name}"
   shared_key                    = "ItsASecret"
   peer_asn                      = 100
   primary_peer_address_prefix   = "192.168.1.0/30"
@@ -79,11 +96,11 @@ resource "azurerm_express_route_circuit_peering" "example" {
   vlan_id                       = 100
 }
 
-resource "azurerm_express_route_connection" "example" {
+resource "azurerm_express_route_connection" "er_connection" {
   for_each = azurerm_express_route_gateway.ergateway
   name                             = "example-expressrouteconn"
   express_route_gateway_id         = "${azurerm_express_route_gateway.ergateway[each.key].id}"
-  express_route_circuit_peering_id = "${azurerm_express_route_circuit_peering.example[each.key].id}"
+  express_route_circuit_peering_id = "${azurerm_express_route_circuit_peering.er_peering[each.key].id}"
 }
 
 resource "azurerm_virtual_network" "Transithubs" {
@@ -120,7 +137,6 @@ resource "azurerm_subnet" "azfwmanagementsubnet" {
 }
 
 
-
 data "azurerm_resource_group" "resource_groups" {
   for_each = var.Transithubs
   name = azurerm_virtual_network.Transithubs[each.key].resource_group_name
@@ -151,10 +167,10 @@ resource "azurerm_public_ip" "fw_management_publicip" {
 }
 
 
-resource "azurerm_firewall" "example" {
+resource "azurerm_firewall" "AZFW" {
   for_each = azurerm_virtual_network.Transithubs
   name                = "testfirewall"
-  location            = data.azurerm_resource_group.resource_groups[each.key].location
+  location            =  each.value.location #data.azurerm_resource_group.resource_groups[each.key].location
   resource_group_name = azurerm_subnet.azfwsubnet[each.key].resource_group_name
   sku_name            = "AZFW_VNet"
   sku_tier            = "Standard"
@@ -169,5 +185,52 @@ resource "azurerm_firewall" "example" {
     name = "configuration"
     subnet_id = azurerm_subnet.azfwmanagementsubnet[each.key].id
     public_ip_address_id = azurerm_public_ip.fw_management_publicip[each.key].id
+  }
+}
+
+
+resource "azurerm_virtual_hub_route_table_route" "default_rt_east_routes" {
+  for_each = var.default_rt_east_routes
+  resource_group_name = "myVWAN_hub1"
+  virtual_hub_name = "vwanhub-eastus2-rg"
+  route_table_id = data.azurerm_virtual_hub.vhub["Hub1_east"].default_route_table_id
+  name              = each.value.name
+  destinations_type = each.value.destinations_type
+  destinations      = each.value.destinations
+  next_hop_type     = "ResourceID"
+  next_hop          = azurerm_virtual_hub_connection.vhub_connection.id
+  }
+
+
+resource "azurerm_virtual_hub_route_table_route" "central_rt_east_routes" {
+  for_each = var.default_rt_central_routes
+  resource_group_name = "myVWAN_hub1"
+  virtual_hub_name = "vwanhub-central-rg"
+  route_table_id = data.azurerm_virtual_hub.vhub["Hub2_central"].default_route_table_id
+  name              = each.value.name
+  destinations_type = each.value.destinations_type
+  destinations      = each.value.destinations
+  next_hop_type     = "ResourceID"
+  next_hop          = azurerm_virtual_hub_connection.vhub_connection.id
+  }
+
+
+resource "azurerm_virtual_hub_connection" "Transithubs_vnet_connections" {
+for_each = azurerm_virtual_network.Transithubs
+  name                      = "example-vhubconn"
+  virtual_hub_id            = azurerm_virtual_hub.example.id
+  remote_virtual_network_id = azurerm_virtual_network.example.id
+
+  routing {
+    associated_route_table_id = azurerm_virtual_hub_route_table.example.id
+    propagated_route_table  {
+      labels = "my-static-route"
+      route_table_ids = ""
+    }
+    static_vnet_route {
+      name = "my-static-route"
+      address_prefixes = ["10.0.0.0/16"]
+      next_hop_ip_address = "10.0.0.1"
+    }
   }
 }
--- variables.tf
+++ variables.tf
@@ -2,19 +2,33 @@ variable "resource_groups" {
   type = map(object({
     name = string
     location = string
+    subscription = string
   }))
   default = {
-    "ResourceGroup-VWAN" = {
-        name = "VWAN-east-rg"
+    "vwan-rg" = {
+        name = "VWAN-rg"
         location = "eastus2"
+        subscription = "true"
     }
-     "ResourceGroup-VWANhub-east" = {
-        name = "VWANhub-east-rg"
+     "vwanhub-eastus2-rg" = {
+        name = "vwanhub-eastus2-rg"
         location = "eastus2"
+        subscription = "true"
     }
-     "ResourceGroup-VWANhub-central" = {
+     "vwanhub-central-rg" = {
         name = "Vwanhub-central-rg"
         location = "centralus"
+        subscription = "true"
+    }
+    "dmzspoke1-eastus2-rg" = {
+        name = "dmzspoke1-east-rg"
+        location = "eastus2"
+        subscription = "true"
+    }
+    "dmzspoke1-central-rg" = {
+        name = "dmzspoke1-central-rg"
+        location = "centralus"
+        subscription = "true"
     }
   }
 }
@@ -22,8 +36,8 @@ variable "resource_groups" {
 variable "vwan" {
     type = map
     default = {
-        name = "myVWAN"
-        rgname = "VWANhub-east-rg"
+        name = "VWAN"
+        rgname = "VWAN-east-rg"
         location = "eastus2"
     }
 }
@@ -34,41 +48,25 @@ variable "vhub" {
     location = string
     rgname = string
     address_prefix = string
+    subscription = string
   }))
   default = {
-    "Hub1" = {
+    "Hub1_east" = {
         name = "myVWAN_hub1"
         rgname = "myRG"
         location = "eastus2"
         address_prefix = "10.0.0.1/24"
+        subscription = "iii"
     }
-    "Hub2" = {
+    "Hub2_central" = {
         name = "myVWAN_hub2"
         rgname = "myRG"
         location = "eastus2"
         address_prefix = "100.0.0.1/24"
+        subscription = "yyy"
     }
   }
 }
-
-/* 
-variable "vhub_test" {
-  type = map
-  default = { 
-        name = "myVWAN_hub1"
-        rgname = "myRG"
-        location = "eastus2"
-        address_prefix = "10.0.0.1/24"
-  }
-  {
-        name = 
-        rgname = 
-        location
-        address_prefix = ""
-  }
-}
-
-*/
 
 variable "route" {
   type = map(object({
@@ -83,24 +81,86 @@ variable "route" {
   }
 }
 
+variable "default_rt_east_routes" {
+  type = map(object({
+    name = string
+    destinations_type = string
+    destinations = list(string)
+    next_hop_type = string
+  }))
+  default = {
+    "route1": {
+      name = "test"
+      destinations_type = "CIDR"
+      destinations = ["10.1.1.1/16"]
+      next_hop_type = "ResourceID"
+    },
+    "route2": {
+      name = "test2"
+      destinations_type = "CIDR"
+      destinations = ["10.1.1.2/16"]
+      next_hop_type = "ResourceID"
+    }
+  }
+}
+
+/* 
+variable "default_rt_east_routes2" {
+  type = list(object({
+    name = string
+    destinations_type = string
+    destinations = list(string)
+    next_hop_type = string
+    next_hop = string
+    next_hop_ip_address = string
+  }))
+  default = [{
+    name = "stuff1"
+    destinations = [ "10.1.1.1" ]
+    destinations_type = "CIDR"
+    next_hop = "value"
+    next_hop_type = "ResourceID"
+  },
+  {
+      name = "test2"
+      destinations_type = "CIDR"
+      destinations = ["10.1.1.1"]
+      next_hop_type = "ResourceID"
+      next_hop_ip_address = "1.1.1.1"
+      next_hop = ""
+  }]
+}
+*/
+
 
 variable "ergateway" {
   type = map(object({
     name = string
     resource_group_name = string
     location = string
+    bgp_peer_asn = string
+    bgp_primary_peer_address_prefix = string
+    bgp_secondary_peer_address_prefix = string
+    bgp_vlan_id = number
   }))
   default = {
-    "gateway-eus" = {
-        name = "test-eus2"
+    "ergateway-eus2" = {
+        name = "EXPRESSROUTE_GATEWAY_NAME"
         resource_group_name = "MyRG"
         location = "eastus2"
-
+        bgp_peer_asn = "100"
+        bgp_primary_peer_address_prefix = "192.168.1.0/30"
+        bgp_secondary_peer_address_prefix = "192.168.2.0/30"
+        bgp_vlan_id = 100
     }
-        "gateway-cus2" = {
-        name = "test-cus"
+        "ergateway-cus" = {
+        name = "EXPRESSROUTE_GATEWAY_NAME"
         resource_group_name = "MyRG"
         location = "centralus"
+        bgp_peer_asn = "100"
+        bgp_primary_peer_address_prefix = "192.168.3.0/30"
+        bgp_secondary_peer_address_prefix = "192.168.4.0/30"
+        bgp_vlan_id = 100
     }
   }
 }
@@ -137,5 +197,5 @@ variable "Transithubs" {
         azfw_mgmt_subet_address_prefix = ["10.0.1.100/27"]
   }
 }
-
+}
 
